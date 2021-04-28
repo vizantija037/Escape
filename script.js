a5fe8bf042d6823289671bfdd79e235c03cf919d
vizantija037 <80783335+vizantija037@users.noreply.github.com>
2021-04-28 10:18:25 +0200

    Update script.js
    Mail validation JS

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ window.onload= function () {
     window.scrollTo(0, 0);
 };
 
+
+let submit = document.getElementById('submit');
+let messageBox = document.getElementById('messageInput');
+let nameBox = document.getElementById('nameInput');
+
+submit.addEventListener('click', validateMessage);
+
+function validateMessage(){
+    if(messageBox.value !== '' && nameBox.value !== ''){
+        alert('\nMessage sent!\n\nThank you for time!');
+    } else {
+        alert('\nMessage NOT sent!!!\n\nFill in all the fields in the contact form!');
+}}
